566d558a46ee77804d310302abe7b8b7f658a0f7
HSE Institute UK <abodxxx925@gmail.com>
2025-07-28 11:40:44 -0700

    Update database.js

--- database.js
+++ database.js
@@ -22,13 +22,13 @@ const certificateDatabase = {
     college: "Magdalen College",
     date: "5 December 2021",
     classification: "With Honors"
-  },
-  "OXFD-2021-65881": {
-    name: "Kaleem Ullah",
-    number: "OXFD-2021-65881",
-    degree: "HSE Health, Safety, and Environment",
+   };
+   "OXFD-2021-12345": {
+    name: "Sophia Chaudhry",
+    number: "OXFD-2021-12345",
+    degree: "Doctor of Philosophy in Biological Sciences",
     college: "Magdalen College",
-    date: "23 June 2025",
+    date: "5 December 2021",
     classification: "With Honors"
-  }
-};
+   {
+ };
